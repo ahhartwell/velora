298f86de5e3a09ebca444ee134483e4f12659779
ahhartwell <ahmedhassib1974@gmail.com>
2026-08-06 00:54:11 +0300

    Add files via upload

---
+++ storageManager.js
@@ -0,0 +1,40 @@
+const StorageManager = {
+
+    set(key, value) {
+
+        localStorage.setItem(
+            key,
+            JSON.stringify(value)
+        );
+
+    },
+
+    get(key) {
+
+        const value = localStorage.getItem(key);
+
+        if (!value) {
+
+            return null;
+
+        }
+
+        return JSON.parse(value);
+
+    },
+
+    remove(key) {
+
+        localStorage.removeItem(key);
+
+    },
+
+    clear() {
+
+        localStorage.clear();
+
+    }
+
+};
+
+window.StorageManager = StorageManager;
