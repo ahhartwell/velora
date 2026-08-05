51bcede1b71a815024569a3d169dced64caf5804
ahhartwell <ahmedhassib1974@gmail.com>
2026-08-06 01:25:55 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv");
 
-const analyzeRoute = require("./routes/analyzeRoute");
-
+const analyzeRoute = require("./analyzeRoute");
+const healthRoute = require("./healthRoute");
 
 dotenv.config();
 
-
 const app = express();
 
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname)));
 
 app.use("/api", analyzeRoute);
+app.use("/api", healthRoute);
 
+app.get("/", (req, res) => {
 
+    res.sendFile(path.join(__dirname, "index.html"));
+
+});
 
 const PORT = process.env.PORT || 3000;
 
-
 app.listen(PORT, () => {
 
-    console.log(`Velora server running on port ${PORT}`);
+    console.log("");
+    console.log("====================================");
+    console.log("Velora AI Business Analyst Started");
+    console.log(`Server: http://localhost:${PORT}`);
+    console.log("====================================");
+    console.log("");
 
 });
