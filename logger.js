a83e33d89729f32694a35460cbba55f5c828ef2f
ahhartwell <ahmedhassib1974@gmail.com>
2026-08-05 18:20:15 +0300

    Add files via upload

---
+++ logger.js
@@ -0,0 +1,29 @@
+const Logger = {
+
+    info(message) {
+
+        console.log(
+            `[INFO] ${new Date().toISOString()} - ${message}`
+        );
+
+    },
+
+    error(message) {
+
+        console.error(
+            `[ERROR] ${new Date().toISOString()} - ${message}`
+        );
+
+    },
+
+    warn(message) {
+
+        console.warn(
+            `[WARN] ${new Date().toISOString()} - ${message}`
+        );
+
+    }
+
+};
+
+window.Logger = Logger;
