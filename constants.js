4a97886ed892965f1399b981b0e40e32de300fb5
ahhartwell <ahmedhassib1974@gmail.com>
2026-08-05 18:15:12 +0300

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,19 @@
+const Constants = {
+
+    APP_NAME: "Velora",
+
+    APP_VERSION: "1.0.0",
+
+    STORAGE_KEY: "velora-analysis",
+
+    API_BASE: "/api",
+
+    ANALYZE_ENDPOINT: "/analyze",
+
+    HEALTH_ENDPOINT: "/health"
+
+};
+
+Object.freeze(Constants);
+
+window.Constants = Constants;
