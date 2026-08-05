2eb751c8da89f6722f118eaa606f418719ba5528
ahhartwell <ahmedhassib1974@gmail.com>
2026-08-05 18:13:44 +0300

    Add files via upload

---
+++ version.js
@@ -0,0 +1,15 @@
+const Velora = {
+
+    name: "Velora",
+
+    version: "1.0.0",
+
+    author: "Velora Team",
+
+    description: "AI Business Analyst"
+
+};
+
+window.Velora = Velora;
+
+console.log(`${Velora.name} v${Velora.version}`);
