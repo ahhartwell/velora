49ce7540c5d5f09fb685befbccee1369f5995e47
ahhartwell <ahmedhassib1974@gmail.com>
2026-08-06 00:08:11 +0300

    Add files via upload

---
+++ appInfo.js
@@ -0,0 +1,15 @@
+const AppInfo = {
+
+    name: "Velora",
+
+    title: "AI Business Analyst",
+
+    version: "1.0.0",
+
+    description:
+        "Analyze business data, discover problems, opportunities and recommendations using AI."
+
+};
+
+
+window.AppInfo = AppInfo;
